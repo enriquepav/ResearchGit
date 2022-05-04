9e7823a121c51e3d486a7f6f8c4fcf0ab87edaa2
Kath Caillahua <maria.caillahua@globant.com>
2022-05-04 00:27:46 -0500

    add mvvm

--- View/showQuotesViewController.swift
+++ View/showQuotesViewController.swift
@@ -11,16 +11,36 @@ class showQuotesViewController: UIViewController {
 
     @IBOutlet weak var titleQuotes: UILabel!
     @IBOutlet weak var labelQuote: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     var quotes: Quotes = Quotes(anime: "", character: "", quote: "")
     
+    //instanciamos nuestro viewmodel
+    private var quotesViewModel : ViewModelgetQuotes!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViewModel()
+        callToViewModelForUIUpdate()
+    }
+    
+    // este método inicializa el viewmodel
+    func initViewModel(){
+        self.quotesViewModel =  ViewModelgetQuotes()
         
-        getQuotes(){ result in
-            self.quotes = result
+        //observamos el showloading
+        quotesViewModel.showLoading = {
             DispatchQueue.main.async {
-                self.titleQuotes.text = "The quote by " + self.quotes.character + " Anime: " + self.quotes.anime
-                self.labelQuote.text = self.quotes.quote
+                self.titleQuotes.text = ""
+                self.labelQuote.text = ""
+                self.activityIndicator.startAnimating()
+            }
+        }
+        
+        //observamos el hideLoading
+        quotesViewModel.hideLoading = {
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
             }
         }
     }
@@ -29,8 +49,23 @@ class showQuotesViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    // este método recibe la acción del bind data
+    func callToViewModelForUIUpdate(){
+           
+           self.quotesViewModel.bindQuotesViewModelToController = {
+               // al completar la respuesta se ejete un update data ya que es un observer
+               self.updateData()
+           }
+    }
     
-
+    func updateData(){
+        DispatchQueue.main.async {
+            // aquí accedemos a la data que se queda almacenada en el viewmodel en nuestra variable "quoteData"
+            // de esa forma usamos el contenido y solo mostramos los datos que necesitamos
+            self.titleQuotes.text = "The quote by " + self.quotesViewModel.quoteData.character + " Anime: " + self.quotesViewModel.quoteData.anime
+            self.labelQuote.text = self.quotesViewModel.quoteData.quote
+        }
+    }
 }
     
     /*
